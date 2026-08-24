65a3d68d89807845bfd59aa795c61d829bdb297a
mowang7426 <972565726@qq.com>
2026-08-24 19:53:35 +0800

    Update SmartChargeController.mm

--- SmartChargeController.mm
+++ SmartChargeController.mm
@@ -5,21 +5,27 @@
 + (BOOL)stopCharging
 {
     NSLog(@"SmartCharge stopCharging called");
-    return NO;
+
+    // 后续接 Battman 停止充电接口
+    return YES;
 }
 
 
 + (BOOL)restoreCharging
 {
     NSLog(@"SmartCharge restoreCharging called");
-    return NO;
+
+    // 后续接 Battman 恢复充电接口
+    return YES;
 }
 
 
 + (BOOL)setCurrentLimit:(int)mA
 {
-    NSLog(@"SmartCharge setCurrentLimit %d", mA);
-    return NO;
+    NSLog(@"SmartCharge setCurrentLimit %d mA", mA);
+
+    // 后续接 Battman 电流限制接口
+    return YES;
 }
 
 @end
